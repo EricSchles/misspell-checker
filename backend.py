afda2c40ed20b4316e00e272d65377253b2a13db
EricSchles <ericschles@gmail.com>
2014-11-14 19:11:47 -0800

    on last function

--- backend.py
+++ backend.py
@@ -25,6 +25,7 @@ class MissSpell:
         else:
             print "You have some how gotten around setting a level, we cannot proceed without this"
             sys.exit(0)
+
     def low_checker(self,text):
         words = text.split(' ')
         words_to_misspell = int(len(words)*(.01*self.percentage))
@@ -62,9 +63,120 @@ class MissSpell:
                     new_word.append(vowel_mapping[letter])
                 else:
                     if ind%3==0 or ind%4==1:
-                    
+                        new_word.append(consonants_mapping[letter])
                     else:
                         new_word.append(letter)
+            words[index] = ''.join(new_word)
             count += 1
 
-            
+        return ''.join(words)
+
+    def swap(self,first,second):
+        return second,first
+        
+
+    def medium_checker(self,text):
+        words = text.split(' ')
+        words_to_misspell = int(len(words)*(.01*self.percentage))
+        if words_to_misspell == 0: words_to_misspell = 1
+        count = 0
+        indexes_used = []
+        while count < words_to_misspell:
+            index = random.randint(0,len(words)-1)
+            indexes_used.append(index)
+            if index in indexes_used:
+                index = random.randint(0,len(words)-1)
+                while index in indexes_used:
+                    indexes_used.append(index)
+                    index = random.randint(0,len(words)-1)
+            word = words[index]
+            characters = [x for x in word]
+            vowels = [x for x in "aeiou"]
+            consonants = [x for x in "bcdfghjklmnpqrstvxzwy"]
+            vowel_mapping = {"a":"e",
+                             "i":"o",
+                             "u":"a",
+                             "o":"u",
+                             "e":"i"
+                             }
+            consonants_mapping ={"l":"r",
+                                 "t":"v",
+                                 "q":"w",
+                                 "x":"z",
+                                 "b":"r",
+                                 "k":"l",
+                                 "f":"g"
+                                 "m":"n",
+                                 "v":"s"
+                                 }
+            new_word = []
+            for ind,letter in enumerate(characters):
+                if letter in vowels:
+                    new_word.append(vowel_mapping[letter])
+                else:
+                    if ind%3==0 or ind%4==1:
+                        new_word.append(consonants_mapping[letter])
+                    else:
+                        new_word.append(letter)
+            words[index] = ''.join(new_word)
+            words[index]
+            count += 1
+            for i in xrange(words):
+                offset = random.randint(0,4)
+                words[index+offset],words[index] = self.swap(words[index],words[index+offset])
+                
+        return ''.join(words)
+
+    def high_checker(self,text):
+        words = text.split(' ')
+        words_to_misspell = int(len(words)*(.01*self.percentage))
+        if words_to_misspell == 0: words_to_misspell = 1
+        count = 0
+        indexes_used = []
+        while count < words_to_misspell:
+            index = random.randint(0,len(words)-1)
+            indexes_used.append(index)
+            if index in indexes_used:
+                index = random.randint(0,len(words)-1)
+                while index in indexes_used:
+                    indexes_used.append(index)
+                    index = random.randint(0,len(words)-1)
+            word = words[index]
+            characters = [x for x in word]
+            vowels = [x for x in "aeiou"]
+            consonants = [x for x in "bcdfghjklmnpqrstvxzwy"]
+            vowel_mapping = {"a":"e",
+                             "i":"o",
+                             "u":"a",
+                             "o":"u",
+                             "e":"i"
+                             }
+            consonants_mapping ={"l":"r",
+                                 "t":"v",
+                                 "q":"w",
+                                 "x":"z",
+                                 "b":"r",
+                                 "k":"l",
+                                 "f":"g"
+                                 "m":"n",
+                                 "v":"s"
+                                 }
+            new_word = []
+            for ind,letter in enumerate(characters):
+                if letter in vowels:
+                    new_word.append(vowel_mapping[letter])
+                else:
+                    if ind%3==0 or ind%4==1:
+                        new_word.append(consonants_mapping[letter])
+                    else:
+                        new_word.append(letter)
+            words[index] = ''.join(new_word)
+            words[index]
+            count += 1
+            for i in xrange(words):
+                offset = random.randint(0,4)
+                words[index+offset],words[index] = self.swap(words[index],words[index+offset])
+                
+        return ''.join(words)
+
+        
